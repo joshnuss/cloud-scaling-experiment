206f81aa7e3c6086bb288455b5986ea4e4b94c9d
Josh Nussbaum <joshnuss@gmail.com>
2023-10-09 20:09:10 -0400

    Update mode label for cloud functions

--- function.js
+++ function.js
@@ -2,7 +2,7 @@ import functions from '@google-cloud/functions-framework'
 import handler from './routes.js'
 
 functions.http('main', (req, res) => {
-  req.mode = 'cloud-function'
+  req.mode = 'function'
 
   return handler(req, res)
 })
